6c738f44407c3689bdf86010dc25bfef97a1ba89
Nicholas Abdulla-Sunderland <nicholas.abdulla-sunderland@aareon.com>
2018-04-11 16:57:41 +0100

    initial commit

---
+++ src/app/services/log.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/observable';
+import { of } from 'rxjs/observable/of';
+
+import { Log } from '../models/log';
+
+@Injectable()
+export class LogService {
+  logs: Log[];
+
+  private logSource = new BehaviorSubject<Log>({ id: null, text: null, date: null });
+  selectedLog = this.logSource.asObservable();
+
+  private stateSource = new BehaviorSubject<boolean>(true);
+  stateClear = this.stateSource.asObservable();
+
+  constructor() {
+    // this.logs = [
+    //   {id: '1', text: 'Generated components', date: new Date('12/26/2017 12:54:23') },
+    //   {id: '2', text: 'Added Bootstrap', date: new Date('12/27/2017 09:33:23') },
+    //   {id: '3', text: 'Added logs component', date: new Date('12/27/2017 12:00:00') }
+    // ];
+    this.logs = [];
+  }
+
+  getLogs(): Observable<Log[]> {
+    return of(this.logs);
+  }
+
+  setFormLog(log: Log) {
+    this.logSource.next(log);
+  }
+
+  addLog(log: Log) {
+    this.logs.unshift(log);
+  }
+
+  updateLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if (log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+
+    this.logs.unshift(log);
+  }
+
+  deleteLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if (log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+  }
+
+  clearState() {
+    this.stateSource.next(true);
+  }
+}
